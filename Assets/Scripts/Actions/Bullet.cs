fd3edac5788e189b4e0f58ddc07c869bf2e826b4
Dcha6 <chadavid472@gmail.com>
2023-07-26 16:04:19 -0400

    Shooting action

---
+++ Assets/Scripts/Actions/Bullet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject hiteffect;
+    public float timer;
+
+    public float effectTimer = 5f;
+    public int damage = 10;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        timer += 1.0F * Time.deltaTime;
+        if (timer >= 1)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //TopDownEnemy enemy = other.GetComponent<TopDownEnemy>();
+        //if(enemy != null)
+        //{
+        //    enemy.TakeDamage(damage);
+        //}
+        Destroy(gameObject);
+
+
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        GameObject effect = Instantiate(hiteffect, transform.position, Quaternion.identity);
+        Destroy(effect, effectTimer);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Actions/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public Transform firepoint;
+    public GameObject bullets;
+    public float bulletForce = 20f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shooting();
+        }
+    }
+
+    void Shooting()
+    {
+        GameObject bullet = Instantiate(bullets, firepoint.position, firepoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firepoint.up * bulletForce, ForceMode2D.Impulse);
+    }
+}
